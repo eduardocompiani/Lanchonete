e30e303e179a6b83d42cf56aa846933cb96ba7e0
Eduardo <eduardo@EduardoCompiani.EduardoCompiani>
2017-11-19 14:34:32 -0200

    updated combo rest

--- src/main/java/br/univille/lanchonetes/ComboRest.java
+++ src/main/java/br/univille/lanchonetes/ComboRest.java
@@ -8,7 +8,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -28,30 +30,86 @@ public class ComboRest {
 		try{
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery(sqlCombo);
-			String sqlProduto = "SELECT PRODUTO.CDPRODUTO, PRODUTO.NMPRODUTO, (((100 - COMBO.VLDESCONTO) / 100) * PRODUTO.VLPRODUTO) AS VLPRODUTO FROM COMBO INNER JOIN PRODUTO_DO_COMBO ON (COMBO.CDCOMBO = PRODUTO_DO_COMBO.CDCOMBO) INNER JOIN PRODUTO ON (PRODUTO.CDPRODUTO = PRODUTO_DO_COMBO.CDPRODUTO) WHERE PRODUTO_DO_COMBO.CDCOMBO = ?";
 			while(rs.next()){
 				Combo combo = new Combo();
 				combo.setCdCombo(rs.getInt(1));
 				combo.setNmCombo(rs.getString(2));
 				combo.setVlDesconto(rs.getFloat(3));
 				combo.setVlCombo(rs.getFloat(4));
-				PreparedStatement stmtProduto = conn.prepareStatement(sqlProduto);
-				stmtProduto.setInt(1, combo.getCdCombo());
-				ResultSet rsProduto = stmtProduto.executeQuery();
-				List<Produto> produtos = new ArrayList();
-				while(rsProduto.next()){
-					Produto produto = new Produto();
-					produto.setCdProduto(rsProduto.getInt(1));
-					produto.setNmProduto(rsProduto.getString(2));
-					produto.setVlProduto(rsProduto.getFloat(3));
-					produtos.add(produto);
-				}
-				combo.setProdutos(produtos);
+				combo.setProdutos(this.pegarProdutoDoCombo(combo.getCdCombo()));
 				combos.add(combo);
 			}
 		}catch(Exception e){
 			e.printStackTrace();
 		}
 		return combos;
+	}
+	
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Combo getCombo(@PathParam("id") int id){
+		Combo combo = new Combo();
+		Connection conn = new DBConnection().openConnection();
+		String sqlCombo = "SELECT CDCOMBO, NMCOMBO, VLDESCONTO, VLCOMBO FROM COMBO WHERE CDCOMBO = ?";
+		try{
+			PreparedStatement stmt = conn.prepareStatement(sqlCombo);
+			stmt.setInt(1, id);
+			ResultSet rs = stmt.executeQuery();
+			if(rs.next()){
+				combo.setCdCombo(rs.getInt(1));
+				combo.setNmCombo(rs.getString(2));
+				combo.setVlDesconto(rs.getFloat(3));
+				combo.setVlCombo(rs.getFloat(4));
+				combo.setProdutos(this.pegarProdutoDoCombo(combo.getCdCombo()));
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return combo;
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public Combo adicionarCombo(Combo combo){
+		String sql = "INSERT INTO COMBO(NMCOMBO, VLDESCONTO, VLCOMBO) (?, ?, ?)";
+		Connection conn = new DBConnection().openConnection();
+		try{
+			PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+			stmt.setString(1, combo.getNmCombo());
+			stmt.setFloat(2, combo.getVlDesconto());
+			stmt.setFloat(3, combo.getVlCombo());
+			stmt.executeUpdate();
+			ResultSet rs = stmt.getGeneratedKeys();
+			if(rs.next()){
+				combo.setCdCombo(rs.getInt(1));
+			}
+			//TO-DO
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
+		return combo;
+	}
+	
+	private List<Produto> pegarProdutoDoCombo(int cdCombo){
+		List<Produto> produtos = new ArrayList();
+		String sqlProduto = "SELECT PRODUTO.CDPRODUTO, PRODUTO.NMPRODUTO, (((100 - COMBO.VLDESCONTO) / 100) * PRODUTO.VLPRODUTO) AS VLPRODUTO FROM COMBO INNER JOIN PRODUTO_DO_COMBO ON (COMBO.CDCOMBO = PRODUTO_DO_COMBO.CDCOMBO) INNER JOIN PRODUTO ON (PRODUTO.CDPRODUTO = PRODUTO_DO_COMBO.CDPRODUTO) WHERE PRODUTO_DO_COMBO.CDCOMBO = ?";
+		Connection conn = new DBConnection().openConnection();
+		try{
+			PreparedStatement stmt = conn.prepareStatement(sqlProduto);
+			stmt.setInt(1, cdCombo);
+			ResultSet rs = stmt.executeQuery();
+			while(rs.next()){
+				Produto produto = new Produto();
+				produto.setCdProduto(rs.getInt(1));
+				produto.setNmProduto(rs.getString(2));
+				produto.setVlProduto(rs.getFloat(3));
+				produtos.add(produto);
+			}
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		return produtos;
 	}
 }
